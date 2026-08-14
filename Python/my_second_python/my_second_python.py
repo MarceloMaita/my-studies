c5d2bbaed1cdd6e3fef4b2a08b748fe38b7cb5ce
Marcelo Maita <84576742+MarceloMaita@users.noreply.github.com>
2026-08-14 10:49:36 -0300

    Add my secon python file

---
+++ Python/my_second_python/my_second_python.py
@@ -0,0 +1,6 @@
+print ("This is my second python file, where I will try to do some math:")
+number = input("Enter the first number:")
+number2 = input("Enter the second number:")
+
+print(f"This is the sum of the two number: {int(number) + int(number2)}")
+
