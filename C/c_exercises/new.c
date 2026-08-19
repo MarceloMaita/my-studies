d349410fef865a2ea4d215aa3b55ced693ac1952
Marcelo Maita <84576742+MarceloMaita@users.noreply.github.com>
2026-08-19 10:40:47 -0300

    Reconfiguration of my folders

---
+++ C/c_exercises/new.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h>
+
+int main()
+{
+    int n;
+
+    //anf("%d", &n);
+    printf("%d", sqrt(16));
+
+    return 0;
+}
---
+++ Python/python_exercises/sqrt.py
@@ -0,0 +1,4 @@
+# Simple example of using the sqrt function in Python.
+import math
+n = float(input())
+print(math.sqrt(n))
