6e86616de324d9acf4930f0f29f3abeb1b359378
Marcelo Maita <84576742+MarceloMaita@users.noreply.github.com>
2026-08-20 11:47:19 -0300

    add c_concepts_double

--- C/c_lab/c_concepts_double.c
+++ C/c_lab/c_concepts_double.c
@@ -16,3 +16,9 @@ int main(void)
 
 /*This code snippet is a simple C program that prints out the maximum finite value of a double, 
 the number of decimal digits of precision, and the number of bits of precision in the significand for the double data type. It uses the constants defined in the float.h header file.*/
+
+/*DBL_MAX
+DBL_MIN
+DBL_DIG
+DBL_MANT_DIG
+DBL_MAX_10_EXP*/
