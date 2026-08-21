79618612b8343919a83453be759a8809d5b13233
Marcelo Maita <84576742+MarceloMaita@users.noreply.github.com>
2026-08-21 12:00:15 -0300

    chage the function to show only the nth term if desired and include a menu.

--- C/c_exercises/fibonacci/fibonacci.c
+++ C/c_exercises/fibonacci/fibonacci.c
@@ -3,46 +3,23 @@
 
 /*write a C program that solve Fibonacci sequence for N terms. Iteratively and recursively.*/
 
-double fibonacci_iterative_way(double predecessor_1, double predecessor_2, int n);//separate function for the iterative approach
+double fibonacci_iterative_way(double predecessor_1, double predecessor_2, int n, int control);//separate function for the iterative approach
 double fibonacci_recursive_way(int n);//separate function for the recursive approach
+void menu(double predecessor_1, double predecessor_2, int n, int control);
 
 int main()
 {
-    int n, i, choice; //both predecessors holds the first two terms of the Fibonacci sequence, term holds the current term, n is the number of terms to be printed, and i is used for iteration.
-    double predecessor_1 = 0, predecessor_2 = 1, term;
-
-    printf("Enter the number of terms in the Fibonacci sequence: ");
-    scanf("%d", &n);
-
-    printf("which way do you want to solve the Fibonacci sequence?\n");
-    printf("1. Iteratively\n");
-    printf("2. Recursively\n");
-  
-    scanf("%d", &choice); 
-    if(choice == 1)
+    int n, i, choice=0; 
+    double predecessor_1 = 0, predecessor_2 = 1, term;//both predecessors holds the first two terms of the Fibonacci sequence, term holds the current term, n is the number of terms to be printed, and i is used for iteration.
+    do
     {
-      fibonacci_iterative_way(predecessor_1, predecessor_2, n);
-    }
-    else if(choice == 2)
-    {
-        //printf("Fibonacci sequence: ");
-        for(i = 1; i <= n; i++)
-        {
-            term = fibonacci_recursive_way(i); //call the recursive function to get the current term
-            printf("%.0f", term); //print the current term
-        }
-    }
-    else
-    {
-        printf("Invalid choice.\n");
-    }
-
+        menu(0, 1, n, 0);
+    }while(choice>=0 && choice<=2);
     
-     
-    
+    return 0;
 }
 
-double fibonacci_iterative_way(double predecessor_1, double predecessor_2, int n)
+double fibonacci_iterative_way(double predecessor_1, double predecessor_2, int n, int control)
 {
     double term;
     int i;
@@ -63,15 +40,22 @@ double fibonacci_iterative_way(double predecessor_1, double predecessor_2, int n
             predecessor_1 = predecessor_2; //update the predecessors for the next iteration
             predecessor_2 = term;
         }
-        printf(" %.0f ", term); //print the current term
+        if(control == 1 && i == n) //if the user wants to see the nth term only, print it
+        {
+            printf(" %.0f ", term); //print the current term
+        }
+        else if(control == 0) //if the user wants to see the Fibonacci sequence for n terms, print each term
+        {
+            printf(" %.0f ", term); //print the current term
+        }
+        
     }
     return 0;
 }
 
 double fibonacci_recursive_way(int n)
 {
-  //testing the error
- //printf("value of n: %d\n", n);
+ 
     if(n == 1) //base case
         return 0;
     else if(n <= 2) 
@@ -79,6 +63,38 @@ double fibonacci_recursive_way(int n)
     else
         return fibonacci_recursive_way(n - 1) + fibonacci_recursive_way(n - 2); //recursive case
 }
-
 /*in the recursive way, I had problems with the first two cases (base cases)
 I solved it, but understand exactly what happened is crucial*/
+
+void menu(double predecessor_1, double predecessor_2, int n, int control)
+{
+    int choice, way;
+    printf("what do you want to do?\n");
+    printf("1. See the fibonacci sequence for n terms?\n");
+    printf("2. See the nth term of the fibonacci sequence?\n");
+    printf("0. Exit\n");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 0:
+            exit(0);   
+        case 1:
+        printf("Enter the number of terms in the Fibonacci sequence: ");
+        scanf("%d", &n);
+            printf("Which way do you want to see the Fibonacci sequence? Iteratively or recursively?\n1-Iteratively\n2-Recursively\n");
+            scanf("%d", &way);
+            fibonacci_iterative_way(0, 1, n, 0); //call the function to see the fibonacci sequence for n terms
+            printf("\n");
+            break;
+        case 2:
+            printf("Enter the position of the term you want to see: ");
+            scanf("%d", &n);
+            fibonacci_iterative_way(0, 1, n, 1); //call the function to see the nth term of the fibonacci sequence
+            printf("\n");
+            break;
+        default:
+            printf("Invalid choice.\n");
+    }
+
+}
