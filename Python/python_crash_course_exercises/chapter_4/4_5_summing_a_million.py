48aad86d4e2b87c0ca6c2a237e17b01561103505
Marcelo Maita <84576742+MarceloMaita@users.noreply.github.com>
2026-08-28 16:25:56 -0300

    Add chapter 4 exercises from python crash course book.

---
+++ Python/python_crash_course_exercises/chapter_4/4_5_summing_a_million.py
@@ -0,0 +1,4 @@
+list = [value for value in range(1, 1000001)]
+print(f"Min: {min(list)}")
+print(f"max: {max(list)}")
+print(f"sum: {sum(list)}")
---
+++ Python/python_crash_course_exercises/chapter_4/4_7_threes.py
@@ -0,0 +1,5 @@
+multiples_of_3 = list(range(0, 31, 3))
+print(multiples_of_3)
+
+for value in multiples_of_3:
+    print(value)
